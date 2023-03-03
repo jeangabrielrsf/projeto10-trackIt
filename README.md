c7d557d66e719b75a87296f6cc71f92b5838c693
Jean Gabriel Frusca <jeang96@gmail.com>
2023-03-03 13:03:29 -0300

    feat: added react-calender on historic page

--- README.md
+++ README.md
@@ -10,12 +10,12 @@ React, Express, Styled Components
 
 ### Back-end:
 
-API ofertada pela Driven Education
+API ofericida pela Driven Education
 
 ### Como rodar o projeto no seu dispositivo:
 
 1. Clonar o repositório
-2. na pasta raiz do repositório, instalar as dependências
+2. Na pasta raiz do repositório, instalar as dependências
    ```bash
    npm install
    ```
--- src/pages/Historic/Historic.js
+++ src/pages/Historic/Historic.js
@@ -4,15 +4,22 @@ import UserProfileImageContext from "../../contexts/UserProfileImageContext";
 import PageTitle from "../../GlobalStyled/PageTitle";
 import Topo from "../../GlobalStyled/Topo";
 import Footer from "../../GlobalStyled/Footer";
+import React, { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
 export default function Historic() {
 	const { userImage } = useContext(UserProfileImageContext);
+	const [value, setValue] = useState(new Date());
+
 	return (
 		<>
 			<Topo source={userImage} />
 			<Wrapper>
 				<PageTitle title={"Histórico"} />
-				<p>Em breve você poderá ver o histórico dos seus hábitos aqui!</p>
+				<CalendarContainer>
+					<Calendar className="calendar" onChange={setValue} value={value} />
+				</CalendarContainer>
 			</Wrapper>
 			<Footer />
 		</>
@@ -22,12 +29,21 @@ export default function Historic() {
 const Wrapper = styled.div`
 	padding: 90px 20px;
 	background-color: #f2f2f2;
-	height: 100vh;
-
+	height: 100%;
 	p {
 		font-size: 18px;
 		line-height: 22.47px;
 		color: #666666;
 		margin: 10px 0px;
+	}
+`;
+
+const CalendarContainer = styled.div`
+	margin-top: 20px;
+	display: flex;
+	justify-content: center;
+
+	.calendar {
+		border-radius: 10px;
 	}
 `;
